c457438b3d168201debdb2908ccbccbffdc78ae4
EDWIN LEONARDO HILARIO GOMEZ <elhilario@correo.url.edu.gt>
2018-06-16 19:49:32 -0600

    CSV

--- ProyectoEstampas/ProyectoEstampas/Form1.Designer.cs
+++ ProyectoEstampas/ProyectoEstampas/Form1.Designer.cs
@@ -28,13 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnExportar = new System.Windows.Forms.Button();
+            this.txtEscribir = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(54, 64);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 0;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnExportar
+            // 
+            this.btnExportar.Location = new System.Drawing.Point(205, 64);
+            this.btnExportar.Name = "btnExportar";
+            this.btnExportar.Size = new System.Drawing.Size(75, 23);
+            this.btnExportar.TabIndex = 1;
+            this.btnExportar.Text = "Exportar";
+            this.btnExportar.UseVisualStyleBackColor = true;
+            this.btnExportar.Click += new System.EventHandler(this.btnExportar_Click);
+            // 
+            // txtEscribir
+            // 
+            this.txtEscribir.Location = new System.Drawing.Point(90, 29);
+            this.txtEscribir.Name = "txtEscribir";
+            this.txtEscribir.Size = new System.Drawing.Size(100, 20);
+            this.txtEscribir.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtEscribir);
+            this.Controls.Add(this.btnExportar);
+            this.Controls.Add(this.btnAgregar);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnExportar;
+        private System.Windows.Forms.TextBox txtEscribir;
     }
 }
 
--- ProyectoEstampas/ProyectoEstampas/Form1.cs
+++ ProyectoEstampas/ProyectoEstampas/Form1.cs
@@ -7,14 +7,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace ProyectoEstampas
 {
     public partial class Form1 : Form
     {
+        StreamWriter Archivo = new StreamWriter("C:\\Test.txt");
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            Archivo.Write(txtEscribir.Text);
+        }
+
+        private void btnExportar_Click(object sender, EventArgs e)
+        {
+            Archivo.Close();
         }
     }
 }
